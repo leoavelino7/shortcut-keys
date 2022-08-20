e00a633fefaa7fda84be7f98bff6c3d6e4fce89a
Leonardo Avelino <>
2022-08-20 13:13:52 -0300

    docs: Remix example

---
+++ examples/remix-example/app/root.tsx
@@ -0,0 +1,40 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import styles from "./styles.css";
+
+import "shortcut-keys";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Shortcut keys - Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
