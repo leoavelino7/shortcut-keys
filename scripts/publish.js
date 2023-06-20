725d3baa01f2e7e0af21a0b1d627994847a2ee7f
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2023-06-20 20:39:25 -0300

    chore: remove .npmrc

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+require("dotenv").config();
 
 const token = process.env.NPM_TOKEN;
 
-const npmrcPath = path.join(__dirname, '.npmrc');
+const npmrcPath = path.join(__dirname, "..", '.npmrc');
 
 fs.writeFileSync(npmrcPath, `//registry.npmjs.org/:_authToken=${token}`);
